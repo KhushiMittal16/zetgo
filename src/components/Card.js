a855afd0826124a207e9daaee4362206e678eefe
khushi <khushi7mittal@gmail.com>
2022-04-25 09:53:11 +0530

    Second Commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import DatePicker from 'react-date-picker';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import DateRangeIcon from '@mui/icons-material/DateRange';
 import { useState } from 'react';
 import '../CSS/main.css';
 
 export default function BasicCard() {
   const [adult, setAdult] = useState(1);
   const [child, setChild] = useState(0);
+  const [chngdate, setChngdate] = useState(new Date());
 
   const handleChangeNegAdult=()=>{
     const adultDec= adult-1;
@@ -34,61 +32,75 @@ export default function BasicCard() {
 
   const handleChangeNegChild=()=>{
     const childDec= child-1;
-    if(childDec>0)
+    if(childDec>=0)
       setChild(childDec);
     else
-      setChild(childDec)
+      setChild(child)
   }
 
+  const dateChng=()=>{
 
+  }
 
   return ( 
     <Card className='checkcard' sx={{ minWidth: 275 }}>
       <CardContent>
+
         <form>
-        <div>
-        <h4>Check-in</h4>
-        <input type="date" placeholder="Enter date"/> 
-        </div>
-        <div>
-        <h4>Check-out</h4>
-        <input type="date" placeholder="Enter date"/> 
-        </div>
+          <div>
+          <div className='head'>Check-in</div>
+          <input type="date" placeholder="Add date"/>
+          {/* <TextField
+            id="date"
+            label="Choose your birthdate"
+            type="date"
+            defaultValue="2017-05-24"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          /> */}
+          {/* <DatePicker onChange={setChngdate} value={chngdate} /> */}
+          {/* <button onChange={setChngdate} value={chngdate}>date</button> */}
+          </div>
+          <div>
+          <div  className='head'>Check-out</div>
+          <input type="date" placeholder="Add date"/>
+          </div>
         </form>
+        
         <div className='guest_search'>
         <div className='guests'>
-        <h4>Guests</h4>
-        {/* <input type="text" placeholder='1 adult'/>  */}
+        <div  className='head'>Guests</div>
         <div className="dropdown">
-            <button className="btn_bootstrap" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fa-solid fa-user"></i> <div>{(child>0)?`${adult} Adult, ${child} Child`: `${adult} Adult`}</div>
+            <button className='dropdownMenu' type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              <i class="fa-solid fa-user"></i> 
+              <div className='typecount'>{(child>0)?`${adult} Adult, ${child} Child`: `${adult} Adult`}</div>
             </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li className='listitem1'>
-                  <div>adult</div>
-                  <div >
-                  <button className='dropdownbtn' onClick={handleChangeNegAdult}>-</button>
-                    {adult}
-                  <button className='dropdownbtn' onClick={handleChangePosAdult}>+</button>
-                  </div>
-                </li>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style={{width:"90%"}}>
+                      <li className='listitem1'>
+                        <div className='guestType'>adult</div>
+                        <div className='incDecbtn'>
+                          <button className='dropdownbtn' onClick={handleChangeNegAdult}><div className='btnneg'>-</div></button>
+                          <div className='count'> {adult}</div>
+                          <button className='dropdownbtn' onClick={handleChangePosAdult}><div className='btnpos'>+</div></button>
+                        </div>
+                      </li>
 
-                <li className='listitem2'> 
-                  <div>children</div>
-                  <div>
-                    <button className='dropdownbtn' onClick={handleChangeNegChild}>-</button>
-                      {child}
-                    <button className='dropdownbtn' onClick={handleChangePosChild}>+</button>
-                  </div>
-                </li>
-            </ul>
+                      <li className='listitem2'> 
+                        <div className='guestType'>children</div>
+                        <div className='incDecbtn'>
+                          <button className='dropdownbtn' onClick={handleChangeNegChild}><div className='btnneg'>-</div></button>
+                            <div className='count'>{child}</div>
+                          <button className='dropdownbtn' onClick={handleChangePosChild}><div className='btnpos'>+</div></button>
+                        </div>
+                      </li>
+            </div>
         </div>
         </div>
         <button className='search'>Search</button>
         </div>
       </CardContent>
       <CardActions>
-        {/* <Button size="small">Learn More</Button> */}
       </CardActions>
     </Card>
   );
